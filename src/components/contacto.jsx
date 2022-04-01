db0dcbf659d5bf5b49d0f9f739fb756e53453774
Edison <edisona.lopezv@utadeo.edu.co>
2022-04-01 16:40:22 -0500

    Agregar formulario

---
+++ src/components/contacto.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+
+<form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
